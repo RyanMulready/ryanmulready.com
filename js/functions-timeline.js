bb96cc29de8e54cbc1ec725a39632bf932b9292f
Ryan Mulready <ryan.mulready@gmail.com>
2017-03-14 14:56:48 -0400

    Base Site commit

---
+++ js/functions-timeline.js
@@ -0,0 +1,88 @@
+(function($) {
+
+	// setting height 0 to .timeline-background
+	$('.timeline-wrapper .timeline-background').css('height', 0);
+
+	// Trigger function on DomReady
+	$(document).ready(function() {
+		timelineColorToggle();
+		
+		setTimeout(timelineHeight, 2000);
+		
+		// Trigger window.scroll, this will initiate some of the scripts
+		$(window).scroll();
+	});
+	
+	// Timeline height function
+	function timelineHeight() {
+		
+		// setting timeline height 50%
+		$('.timeline-wrapper .timeline-background').css('height', 50+'%');
+
+	}
+	
+	// Timeline Magic
+	function timelineColorToggle() {
+
+		var sections = $('section.content-section').reverse(),
+			timelineWrapper = $('.timeline-wrapper'),
+			halfScreen = $(window).height() / 2;
+			docHeight = $(document).height();
+			
+		// Timeline scroll function
+		$(window).on('scroll resize', function() {
+
+			delay(function() {
+
+				var currScroll = $(window).scrollTop() > $(document).scrollTop() ? $(window).scrollTop() : $(document).scrollTop(),
+					scrollSplit = currScroll + halfScreen;
+
+				sections.removeClass('active').each(function() {
+
+					var section = $(this),
+						postOffset = section.offset().top;
+
+					if(scrollSplit > postOffset) {
+
+						// Add active class to fade in
+						section.addClass('active')
+
+						// Get section color
+						var color = section.data('timeline-color') ? section.data('timeline-color') : null,
+							allColors = 'color-green color-yellow color-crimson color-purple color-blue color-dark color-orange'
+
+						timelineWrapper.removeClass(allColors);
+
+						if(color !== null) {
+							timelineWrapper.addClass('color-' + color);
+						}
+
+						return false;
+					}
+
+				});
+				
+			}, 20);
+
+		});
+
+	}
+
+})(jQuery);
+
+
+/*==========  Helpers  ==========*/
+
+// Timeout function
+var delay = (function(){
+	var timer = 0;
+	return function(callback, ms){
+		clearTimeout (timer);
+		timer = setTimeout(callback, ms);
+	};
+})();
+
+// Reverse function
+$.fn.reverse = function() {
+	return this.pushStack(this.get().reverse(), arguments);
+};
---
+++ php/form.php
@@ -0,0 +1,19 @@
+<?php	
+if( isset( $_POST["submit"] ) ) {
+
+	// Email Recepient for Enquiry Form
+	$ToEmail = 'hello@umairrazzaq.com';
+
+	$EmailSubject = 'Inquiry from ' .$_POST["name"];
+	
+	$mailheader = "From: " .$_POST["email"]. "\r\n";
+	$mailheader .= "Reply-To: " .$_POST["email"]. "\r\n";
+	$mailheader .= "Content-type: text/html; charset=iso-8859-1\r\n";
+	$MESSAGE_BODY = "<strong>Name:</strong> " .$_POST["name"]. "<br>";
+	$MESSAGE_BODY .= "<strong>Email:</strong> " .$_POST["email"]. "<br>";
+	$MESSAGE_BODY .= "<strong>Message:</strong> " .nl2br($_POST["comments"]);
+
+	mail($ToEmail, $EmailSubject, $MESSAGE_BODY, $mailheader) or die ("Failure"); 
+	echo "Your mail has been sent successfuly ! Thank you for your feedback";
+}
+?>
