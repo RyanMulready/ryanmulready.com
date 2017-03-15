be5b6c7656f2e580c468ef2b29b82c809b896731
Ryan Mulready <ryan.mulready@gmail.com>
2017-03-15 16:25:14 -0400

    Cleanup unused files

--- js/functions-timeline.js
+++ js/functions-timeline.js
@@ -24,8 +24,6 @@
 				var currScroll = $(window).scrollTop() > $(document).scrollTop() ? $(window).scrollTop() : $(document).scrollTop(),
 					scrollSplit = currScroll + halfScreen;
 				
-				console.log(currScroll);
-				
 				sections.removeClass('active').each(function() {
 
 					var section = $(this),
