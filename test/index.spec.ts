f36a05e79196d1c6db3211f1f7a3e227f61d01c8
Ryan Mulready <ryan.mulready@gmail.com>
2022-09-12 16:21:07 -0400

    Update stylelint, dependabot and add vitest

---
+++ test/index.spec.ts
@@ -0,0 +1,12 @@
+import { describe, test } from 'vitest';
+import { setup } from '@nuxt/test-utils-edge';
+
+describe('test', async () => {
+    await setup({
+        // test context options
+    });
+
+    test('test', () => {
+        // ...
+    });
+});
